942e05c1d5c1509f87043877f420a23fbdb01cb1
Umair Choudhary <umair.choudhary66@gmail.com>
2017-05-17 23:15:21 -0400

    ToDo initialize

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+	res.send('TODO root');
+});
+
+app.listen(PORT, function(){
+	console.log('Listening on ' + PORT);
+});
