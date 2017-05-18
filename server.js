2e95da798e6a55b7d081fd1f9ad3cf3aff50c0a1
Umair Choudhary <umair.choudhary66@gmail.com>
2017-05-17 23:58:55 -0400

    added todos

--- server.js
+++ server.js
@@ -2,9 +2,39 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Make side projects',
+	completed: false
+}, {
+	id: 2,
+	description: 'Apply to WaterlooWorks',
+	completed: false
+}, {
+	id: 3,
+	description: 'Get and interview',
+	completed: true
+}];
+
+
 app.get('/', function(req, res){
 	res.send('TODO root');
 });
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+})
+
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	for(var i = 0; i < todos.length; i++){
+		if(todos[i].id === todoId){
+			res.send(todos[i]);
+			return;
+		}
+	} 
+	res.status(404).send();
+})
 
 app.listen(PORT, function(){
 	console.log('Listening on ' + PORT);
