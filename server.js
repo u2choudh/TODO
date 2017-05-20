5dfa8582818b42dd2f1f25bdd2f031b5abca3c46
Umair Choudhary <umair.choudhary66@gmail.com>
2017-05-20 19:35:48 -0400

    added updating and queryParams

--- server.js
+++ server.js
@@ -11,11 +11,20 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res){
+
 	res.send('TODO root');
 });
 
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+	res.json(filteredTodos);
 })
 
 app.get('/todos/:id', function(req, res){
@@ -58,8 +67,42 @@ app.delete('/todos/:id', function(req, res){
 	}
 })
 
+app.put('/todos/:id', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	} 
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	} 
+	
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+})
+
+
+
 
 
 app.listen(PORT, function(){
 	console.log('Listening on ' + PORT);
 });
+
+
+
+
+
